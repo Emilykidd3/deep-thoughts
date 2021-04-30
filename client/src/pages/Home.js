7acee33c363f311ddcca4cfdc7323297c19c5677
emilykidd3 <e.kidd61@yahoo.com>
2021-04-29 17:24:41 -0700

    added friend request button for other users

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -8,6 +8,8 @@ import Auth from '../utils/auth';
 
 import FriendList from '../components/FriendList';
 
+import ThoughtForm from '../components/ThoughtForm';
+
 const Home = () => {
   const { loading, data } = useQuery(QUERY_THOUGHTS);
   const { data: userData } = useQuery(QUERY_ME_BASIC);
@@ -18,6 +20,11 @@ const Home = () => {
   return (
     <main>
       <div className="flex-row justify-space-between">
+        {loggedIn && (
+          <div className="col-12 mb-3">
+            <ThoughtForm />
+          </div>
+        )}
         <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
           {loading ? (
             <div>Loading...</div>
