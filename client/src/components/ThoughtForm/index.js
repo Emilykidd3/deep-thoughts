79986695aa36ea615909484ea1dd126934685e52
emilykidd3 <e.kidd61@yahoo.com>
2021-04-29 18:07:21 -0700

    added the ability to react to a thought

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,6 +1,32 @@
 import React, { useState } from 'react';
 
+import { useMutation } from '@apollo/react-hooks';
+import { ADD_THOUGHT } from '../../utils/mutations';
+
+import { QUERY_THOUGHTS, QUERY_ME } from '../../utils/queries';
+
 const ThoughtForm = () => {
+    const [ addThought, { error } ] = useMutation(ADD_THOUGHT, {
+        update(cache, { data: { addThought } }) {
+            try {
+                const { thoughts } = cache.readQuery({ query: QUERY_THOUGHTS });
+
+                cache.writeQuery({
+                    query: QUERY_THOUGHTS,
+                    data: { thoughts: [addThought, ...thoughts] }
+                });
+            } catch (e) {
+                console.error(e);
+            }
+
+            const { me } = cache.readQuery({ query: QUERY_ME });
+            cache.writeQuery({
+                query: QUERY_ME,
+                data: { me: { ...me, thoughts: [...me.thoughts, addThought] } }
+            })
+        }
+    });
+
     const [thoughtText, setText] = useState('');
     const [characterCount, setCharacterCount] = useState(0);
 
@@ -13,14 +39,26 @@ const ThoughtForm = () => {
 
     const handleSubmit = async event => {
         event.preventDefault();
-        setText('');
-        setCharacterCount(0);
+
+        try {
+            await addThought({
+                variables: { thoughtText }
+            });
+
+            setText('');
+            setCharacterCount(0);
+        } catch (e) {
+            console.error(e);
+        }
+
+
     }
 
     return (
         <div>
             <p className={`m-0 ${characterCount === 280 ? 'text-error' : ''}`}>
                 Character Count: {characterCount}/280
+                {error && <span className="ml-2">Something went wrong...</span>}
             </p>
             <form className="flex-row justify-center justify-space-between-md align-stretch" onSubmit={handleSubmit}>
                 <textarea placeholder="Here's a new thought..." value={thoughtText} className="form-input col-12 col-md-9" onChange={handleChange}></textarea>
